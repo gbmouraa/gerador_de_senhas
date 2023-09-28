d6b07583c7e18dce3be9b919e52411a63c6b4484
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2023-09-28 18:01:16 -0300

    adiocionando readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Gerador de senhas
+
+Projeto construído utilizando:
+
+- HTML
+- CSS
+- Javascript
+
+## Overview
+
+Projeto simples para treinar minhas skills em Js, neste projeto é possivel criar uma senha que tenha no minimo 6 e no maximo 20 caracteres, selecionar se deseja que seja inserido na senha:
+
+- letras maiusculas
+- letras minusculas
+- numeros
+- simbolos
+
+### Screenshot
+
+![Print do site](./assets/screenshot.png)
+
+#### Autor
+
+Linkedin - ![Gabriel Moura](https://www.linkedin.com/in/gabriel-moura-b63382161/)
+
+Frontend Mentor -  ![@gbmouraa](https://www.frontendmentor.io/profile/gbmouraa)
--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,31 @@
-const rand = (min, max) => Math.floor(Math.random() * (max - min) + min)
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const geraMaiuscula = () => String.fromCharCode(rand(65, 91))
-const geraMinuscula = () => String.fromCharCode(rand(97, 103))
-const geraNum = () => String.fromCharCode(rand(48, 58))
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 103));
+const geraNum = () => String.fromCharCode(rand(48, 58));
 
-const simbolos = '!@#$%&*()_-|<>.,/?][{}'
-const geraSimbolos = () => simbolos[rand(0, simbolos.length)]
+const simbolos = "!@#$%&*()_-|<>.,/?][{}";
+const geraSimbolos = () => simbolos[rand(0, simbolos.length)];
 
 function geraSenha(qtd, maiusculas, minusculas, numeros, simbolos) {
-  const qtdCaracteres = Number(qtd)
-  const senhaArr = []
+  const qtdCaracteres = Number(qtd);
+  const senhaArr = [];
 
   for (let caracteres = 0; caracteres < qtdCaracteres; caracteres++) {
-    maiusculas && senhaArr.push(geraMaiuscula())
-    minusculas && senhaArr.push(geraMinuscula())
-    numeros && senhaArr.push(geraNum())
-    simbolos && senhaArr.push(geraSimbolos())
+    maiusculas && senhaArr.push(geraMaiuscula());
+    minusculas && senhaArr.push(geraMinuscula());
+    numeros && senhaArr.push(geraNum());
+    simbolos && senhaArr.push(geraSimbolos());
   }
 
-  return senhaArr.join('').slice(0, qtdCaracteres)
-
+  return senhaArr.join("").slice(0, qtdCaracteres);
 }
 
-const qtd = document.querySelector('#qtd-caracteres')
-const maiusculas = document.querySelector('#inp-maiusculas')
-const minusculas = document.querySelector('#inp-minusculas')
-const numeros = document.querySelector('#inp-nums')
-const inpSimbolos = document.querySelector('#inp-simbolos')
+const qtd = document.querySelector("#qtd-caracteres");
+const maiusculas = document.querySelector("#inp-maiusculas");
+const minusculas = document.querySelector("#inp-minusculas");
+const numeros = document.querySelector("#inp-nums");
+const inpSimbolos = document.querySelector("#inp-simbolos");
 
 const gera = () => {
   const senha = geraSenha(
@@ -35,18 +34,17 @@ const gera = () => {
     minusculas.checked,
     numeros.checked,
     inpSimbolos.checked
-  )
-  return senha
-}
+  );
+  return senha;
+};
 
 const handleSubmit = (e) => {
-  e.preventDefault()
-  
-  const senha = document.querySelector('.senha-gerada')
-  senha.innerHTML = gera()
-  senha.classList.add('show-senha')
-}
+  e.preventDefault();
 
-const form = document.querySelector('form')
-form.addEventListener('submit', e => handleSubmit(e))
+  const senha = document.querySelector(".senha-gerada");
+  senha.innerHTML = gera();
+  senha.classList.add("show-senha");
+};
 
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => handleSubmit(e));
